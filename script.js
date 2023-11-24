94119ab838e7a2e6e615bfc828edb85456622ab2
SaleenaF <149341057+SaleenaF@users.noreply.github.com>
2023-11-24 17:53:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.querySelector('.theme-button');
-  const body = document.querySelector('body');
-  button.addEventListener('click', function () {
-    const classList = body.classList;
-    if (classList.contains('dark-theme')) {
-      classList.remove('dark-theme');
-      button.innerText = 'Light Theme';
-    } else {
-      classList.add('dark-theme');
-      button.innerText = 'Dark Theme';
-    }
+window.onload = function() {
+  var themeButton = document.querySelector(".theme-button");
+  
+  themeButton.addEventListener("click", function() {
+    document.documentElement.classList.toggle("dark-theme");
+    themeButton.innerText = themeButton.innerText ? "Dark Theme" : "Light Theme";
   });
-});
+};
