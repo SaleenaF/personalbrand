ca7c467cb0ba4b9cfcb967247fee5ab2ffa9f472
SaleenaF <149341057+SaleenaF@users.noreply.github.com>
2023-11-24 17:36:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-.theme {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-decoration: none;
-    font-family: Verdana, sans-serif;
-    font-size: 3<|beginningofdialog|>2px;
-    font-weight: bold;
-    color: #000;
-    background-color: #fff;
-    border: 1px solid #000;
-}
-
-.light {
-    background-color: #fff;
-    color: #000;
-}
-
-.dark {
-    background-color: #000;
-    color: #fff;
-}
-
-.theme.light {
-    background-color: #fff;
-    color: #000;
-}
-
-.theme.dark {
-    background-color: #000;
-    color: #fff;
-}
+window.onload = function() {
+  var themeButton = document.querySelector(".theme-button");
+  
+  themeButton.addEventListener("click", function() {
+    document.documentElement.classList.toggle("dark-theme");
+    themeButton.innerText = themeButton.innerText ? "Dark Theme" : "Light Theme";
+  });
+};
